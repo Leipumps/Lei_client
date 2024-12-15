b5678363eea9b39912be63e0abfcde7d3f6f7e1e
IVANHERALD <ivanheraldd@gmail.com>
2024-12-15 21:07:59 +0530

    Feat:coming soon

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import comingsoon from '../src/asset/cmsoon.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={comingsoon}/>
+    </div>
+  );
+}
+
+export default App;
